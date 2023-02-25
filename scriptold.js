5fe58dadee930e22d65e4a1c10b49602d0cf3f21
mariorecinos <recinos.mario68@gmail.com>
2023-02-25 12:44:26 -0800

    changes

--- scriptold.js
+++ scriptold.js
@@ -105,8 +105,8 @@ cardEls.forEach(function (el, index) {
             } else {
                 canGuess = false;
                 setTimeout(function () {
-                    cardEls[firstGuess].setAttribute('src', '/images/red2.svg')
-                    cardEls[index].setAttribute('src', '/images/red2.svg')
+                    cardEls[firstGuess].setAttribute('src', './images/red2.svg')
+                    cardEls[index].setAttribute('src', './images/red2.svg')
                     firstGuess = null;
                     canGuess = true;
                     // no match
@@ -132,7 +132,7 @@ function resetGame() {
         document.querySelector('#guesses').textContent = guesses
 
         cardEls.forEach(function (el, index) {
-            el.setAttribute('src', '/images/red2.svg')
+            el.setAttribute('src', './images/red2.svg')
         })
 
         cards.forEach(function(card, index){
